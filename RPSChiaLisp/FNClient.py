45fca7daf6fdb10f8edb17f11a57ba25d3c857ed
MrDennis <jesus.dennis@hotmail.com>
2025-01-26 20:02:21 -0700

    feat: Update FNClient to use hardcoded hostname and port for FullNodeRpcClient

--- RPSChiaLisp/FNClient.py
+++ RPSChiaLisp/FNClient.py
@@ -9,7 +9,9 @@ class FNClient:
     @staticmethod
     async def getClient():
         config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
-        self_hostname = config["self_hostname"]
-        full_node_rpc_port = config["full_node"]["rpc_port"]
+        self_hostname = "api.coinset.org"
+        full_node_rpc_port = 443
+        # self_hostname = config["self_hostname"]
+        # full_node_rpc_port = config["full_node"]["rpc_port"]
         client = await FullNodeRpcClient.create(self_hostname, uint16(full_node_rpc_port), DEFAULT_ROOT_PATH, config)
         return client
